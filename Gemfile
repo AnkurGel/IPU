7911def8afd95116c918403ad034f2c91ee3764c
Ankur Goel <ankurgel@gmail.com>
2012-06-10 12:59:43 +0530

    msit edits

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
 #gem 'rubygems'
 gem 'sinatra'
+gem 'nokogiri'
 #gem 'open-uri'
--- run.rb
+++ run.rb
@@ -1,10 +1,30 @@
-%w(sinatra open-uri erb rubygems).each{|x| require x}
+%w(sinatra nokogiri open-uri erb rubygems).each{|x| require x}
 get '/' do
     @results=extract('http://ggsipuresults.nic.in/ipu//results/resultsmain.htm', 'results')
     @datesheets=extract('http://ggsipuresults.nic.in/ipu//datesheet/datesheetmain.htm', 'datesheet')
+    
     erb :index
 end
 def extract(url,tag)
     res=open(url, &:read).scan(/.*<tr\s*>.*\n.*\n.*B.\s?[Tt]ech.*\n.*\n.*\n.*/).map{|x| x.gsub!(/href=\"/,"href=\"http://ggsipuresults.nic.in/ipu//#{tag}/")} #power or ruby!
     res
+end
+class Array; def first3; self.first(3); end; end
+def msit
+    begin 
+    url="http://msit.in/newscolumn.aspx"
+    doc=Nokogiri::HTML(open(url))
+    res=doc.css("#ctl00_ContentPlaceHolder1_Panel1 small , .NewsClass")
+    rescue Exception
+        STDERR.puts "Trouble with connection with IPU site - #{$!}"
+        raise 
+    end
+    dates, notices, links=[],[],[]
+    res.each do |x| 
+        dates<<x.text
+        links<<x[:href]
+    end
+    links.compact!
+    notices=dates[dates.length/2..dates.length]; dates-=notices
+    [links, notices, dates].map &:first3
 end
