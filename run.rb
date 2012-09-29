797062797203228c3d995098614a2fd947c0c619
Ankur Goel <ankurgel@gmail.com>
2012-09-30 01:02:57 +0530

    minor changes. Will dump and work on this app again

--- run.rb
+++ run.rb
@@ -1,10 +1,11 @@
-%w(sinatra open-uri erb rubygems).each{|x| require x}
+%w(sinatra open-uri erb rubygems debugger).each{|x| require x}
 get '/' do
-    @results=extract('http://ggsipuresults.nic.in/ipu//results/resultsmain.htm', 'results')
-    @datesheets=extract('http://ggsipuresults.nic.in/ipu//datesheet/datesheetmain.htm', 'datesheet')
+    @results = extract('http://ggsipuresults.nic.in/ipu//results/resultsmain.htm', 'results')
+    @datesheets = extract('http://ggsipuresults.nic.in/ipu//datesheet/datesheetmain.htm', 'datesheet')
     erb :index
 end
 def extract(url,tag)
-    res=open(url, &:read).scan(/.*<tr\s*>.*\n.*\n.*B.\s?[Tt]ech.*\n.*\n.*\n.*/).map{|x| x.gsub!(/href=\"/,"href=\"http://ggsipuresults.nic.in/ipu//#{tag}/")} #power or ruby!
+    res=open(url, &:read).scan(/.*<tr\s*>.*\n.*\n.*B.\s?[Tt]ech.*\n.*\n.*\n.*/).map{|x| x.gsub!(/href=\"/,"href=\"http://ggsipuresults.nic.in/ipu//#{tag}/")} 
+    debugger
     res
 end
